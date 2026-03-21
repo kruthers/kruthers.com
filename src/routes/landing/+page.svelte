d00d69fed7b40809ee684ac9778f05e249e171a6
kruthers <pandakruthers@gmail.com>
2026-03-21 16:43:31 +0000

    (Feat) Landing redirect page

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+</script>
+
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center items-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Service No Longer Online</h1>
+            <p class="py-6">
+                Hello, the url you service/ files you went to are not longer online/ active. Dont worry  the data still exists,
+                please contact me via discord and ill send you an archive. I have just been cleaning out old service and moving things around.
+                <br>
+                Past events beside, no data has been removed and is still accessible, whoever you are
+                <br><br>
+                The files will be sent straight over ass long as you used to have access i send it over, no questions, no strings attached.
+                <br>
+                Contact details below
+            </p>
+            <div class="flex gap-3 w-full">
+                <div class="avatar">
+                    <Icon icon="skill-icons:discord" width="2em" height="2em" />
+                </div>
+                <div>
+                    <div class="font-bold">Jordan</div>
+                    <div class="text-sm opacity-50">@kruthers</div>
+                </div>
+                <div>
+                    <Icon icon="mdi:email-outline" width="2em" height="2em" />
+                </div>
+                <div>
+                    <h3 class="font-lg font-bold">pandakruthers@gmail.com</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
